9760b4a4ff1f95bd2048d379e157c10408a6622b
krishnakaveri <krishnaaky142000@gmail.com>
2023-05-05 15:44:43 +0530

    day_2work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { Route,Routes, Navigate } from "react-router-dom";
+
+import LowerNavbar from "./Components/Navbar/LowerNavbar";
+import Footer from "./Components/Footer/Footer"
+import NewNavbar from "./development_test/dummyNavbar/test/NewNavbar";
+// import LandingPage from "./Components/LandingPage/LandingPage";
+import Extranavbar from "./Components/Navbar/Extranavbar";
+
+import Login from "./Components/Login1/Login1/Login";
+import SignUp from "./Components/Login1/SignUp/SignUp";
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <Extranavbar/>
+      <LowerNavbar/>
+     
+       <Routes>
+          
+         
+   
+      {/* <Route path="payment" element={<Payment/>}></Route> */}
+     
+      <Route path="login" element={<Login/>}></Route>
+      <Route path="signup" element={<SignUp/>}></Route>
+       </Routes>
+       <Footer/>
+       {/* </Router> */}
     </div>
   );
 }
